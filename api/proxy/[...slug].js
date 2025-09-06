d00ab567b9a541a6985e798110a13129a6ea25b7
Danin12312 <89481989+Danin12312@users.noreply.github.com>
2025-09-06 11:30:30 +0200

    Update [...slug].js

--- api/proxy/[...slug].js
+++ api/proxy/[...slug].js
@@ -1,4 +1,6 @@
 export default async function handler(req, res) {
+    console.log("Received request:", req.method, req.url, req.body);
+    res.status(200).json({ ok: true });
   try {
     const { slug } = req.query;
     const targetUrl = `https://prod.softswiss.bet/${slug.join('/')}`;
