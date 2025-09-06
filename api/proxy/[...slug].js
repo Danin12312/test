63456bd3a229a892a95015c9523ecf3c76c180b9
Danin12312 <89481989+Danin12312@users.noreply.github.com>
2025-09-06 11:22:09 +0200

    Update [...slug].js

--- api/proxy/[...slug].js
+++ api/proxy/[...slug].js
@@ -1,10 +1,10 @@
-export default async function handler(req, res) {
+// api/proxy/[...slug].js
+module.exports = async (req, res) => {
   try {
-    // Just echo the slug for now
     const { slug } = req.query;
     return res.status(200).json({ slug, method: req.method });
   } catch (err) {
     console.error(err);
     return res.status(500).json({ error: 'Function crashed', details: err.message });
   }
-}
+};
