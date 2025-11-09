df72bf88c48b9a50b636974d326326162375ab7c
Danin12312 <89481989+Danin12312@users.noreply.github.com>
2025-11-09 12:20:33 +0100

    Update gettoken.js

--- api/gettoken.js
+++ api/gettoken.js
@@ -3,6 +3,26 @@
 import { Redis } from 'ioredis';
 import { randomUUID } from 'crypto';
 
+function setCORSHeaders(req, res) {
+  // ------------------------------------------------------------------
+  // ⚠️ YOU MUST UPDATE THIS LINE ⚠️
+  //
+  // Put your *exact* frontend domain here.
+  // If you ran "npx serve", this will be 'http://localhost:3000'
+  //
+  const origin = 'https://test-1wfy.vercel.app/'; // <-- ⚠️ UPDATE THIS
+  //
+  // ------------------------------------------------------------------
+  
+  res.setHeader('Access-Control-Allow-Credentials', 'true');
+  res.setHeader('Access-Control-Allow-Origin', origin); // Must be a specific domain
+  res.setHeader('Access-Control-Allow-Methods', 'GET,POST,OPTIONS');
+  res.setHeader(
+    'Access-Control-Allow-Headers',
+    'X-Requested-With, Content-Type, Accept, Authorization'
+  );
+}
+
 // Initialize client using the REDIS_URL
 const client = new Redis(process.env.REDIS_URL);
 
